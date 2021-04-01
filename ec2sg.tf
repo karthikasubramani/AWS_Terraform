918fecc58e926b46eb101229e0e34f0f87ab1dad
karthikasubramani <62528753+karthikasubramani@users.noreply.github.com>
2021-04-01 17:04:37 +0530

    Create ec2sg.tf

---
+++ ec2sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-8de0fde5"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
