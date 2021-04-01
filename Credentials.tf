72a7b882f3dc79b0bb532dbf0fc338c0472d536c
karthikasubramani <62528753+karthikasubramani@users.noreply.github.com>
2021-04-01 16:54:01 +0530

    Create Credentials.tf

---
+++ Credentials.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = "jhhkhklkhkhkhkhkhhh"
+}
+variable "aws_secret_key" {
+default = "A6n9quhhhhhhhhhW1OFlriiiiiiiii1"
+}
+variable "aws_region" {
+default = "ap-south-1"
+}
